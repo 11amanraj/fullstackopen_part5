befcd9ce3458a1ec67d0b929db35aa855afb889d
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-12-02 23:50:24 +0530

    5.3: bloglist frontend, step3

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
 }
 
-export default { getAll }
+const createNew = async (obj) => {
+  const config = {
+    headers: { Authorization: obj.token }
+  }
+  const response = await axios.post(baseUrl, obj.blog, config)
+  return response.data
+}
+
+const blogService = { getAll, createNew }
+
+export default blogService
