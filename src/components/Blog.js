3bb1d282925a47a86ec85f2540c00f3f53761ee6
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-12-03 16:43:12 +0530

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const detailDisplayHandler = () => {
+    if(showDetail === false) setShowDetail(true)
+    if(showDetail === true) setShowDetail(false)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}> 
+      {blog.title} by {blog.author} <button onClick={detailDisplayHandler}>{showDetail ? 'hide' : 'show'}</button>
+      {showDetail && (
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes}<button>like</button></p>
+          {/* earlier blogs do not have users remove blog.user check after adding delete functionality */}
+          {blog.user && <p>{blog.user.name}</p>}
+        </div>
+      )}
+    </div>
+  )  
+}
 
 export default Blog
