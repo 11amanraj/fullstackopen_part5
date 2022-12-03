73a3cedf0dadeea5abb4d82a37f588ec52e67d18
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-12-03 23:56:16 +0530

    5.14: Blog list tests, step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('only title and author are rendered', () => {
@@ -27,4 +28,40 @@ test('only title and author are rendered', () => {
 
   const likesElement = screen.queryByText(50)
   expect(likesElement).toBeNull()
+})
+
+test('likes and url are rendered when button is clicked', async () => {
+  const blog = {
+    title: 'Realm of Elderlings',
+    author: 'Robin Hobb',
+    url: 'www.elderlings.com',
+    likes: 50,
+    id: 4565,
+    user: {
+      id: 8945,
+      name: 'Wagatha',
+      username: 'christie',
+    }
+  }
+
+  const userID = {
+    id: 8945,
+    name: 'Wagatha',
+    username: 'christie',
+  }
+
+  render(<Blog blog={blog} user={userID}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('show')
+  await user.click(button)
+
+  const elementTitle = screen.getByText('Realm of Elderlings by Robin Hobb')
+  expect(elementTitle).toBeDefined()
+
+  const urlElement = screen.queryByText('www.elderlings.com')
+  expect(urlElement).toBeDefined()
+
+  const likesElement = screen.queryByText(50)
+  expect(likesElement).toBeDefined()
 })
